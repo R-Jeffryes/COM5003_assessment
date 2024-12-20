71797f1d5cbe2ab8f3a97c9fde06da36ec4a39a7
R-Jeffryes <117273119+R-Jeffryes@users.noreply.github.com>
2024-12-20 10:01:49 +0000

    Add files via upload

---
+++ com.mycompany_Project_jar_1.0-SNAPSHOT_1/src/main/java/com/mycompany/project/Lab_Tasks/Session_1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.project.Lab_Tasks;
+import java.util.Scanner;
+/**
+ *
+ * @author aws.admin
+ */
+public class Session_1 {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        String UserInput;
+        
+        UserInput = input.nextLine();
+        System.out.println(UserInput.toUpperCase());
+    }
+}
